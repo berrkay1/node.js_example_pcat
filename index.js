af7b015353359d0a092d1c4ae19c8a2064f879b6
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-08 01:57:39 +0300

    templete engine

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 
+app.set('view engine', 'ejs');
+
+//middleweare
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+ // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+ res.render("index");
 });
+
+app.get('/about', function (req, res) {
+  
+  res.render("about");
+ });
+
+ app.get('/contact', function (req, res) {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("contact");
+ });
 
 app.listen(3000, () => {
   console.log('sunucu çalıştı');
