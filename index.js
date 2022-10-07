53d4a3ee2706df68126354efe328cb0afa6a54fc
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-08 02:52:31 +0300

    ejs start

--- index.js
+++ index.js
@@ -4,15 +4,27 @@ const ejs = require('ejs');
 
 const app = express();
 
+
+// temlete engine
 app.set('view engine', 'ejs');
 
 //middleweare
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
- // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+ 
  res.render("index");
 });
+
+app.get('/about', function (req, res) {
+ 
+  res.render("about");
+ });
+
+ app.get('/add', function (req, res) {
+ 
+  res.render("add");
+ });
 
 app.get('/about', function (req, res) {
   
