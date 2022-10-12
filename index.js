10b03f0cdffcdc215190a14ee00be5aee0ea72b7
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-13 00:00:33 +0300

    mangoo

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 const ejs = require('ejs');
+const Photo = require("./modals/Photo");
+
 
 const app = express();
 
+
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 // temlete engine
 app.set('view engine', 'ejs');
 
 //middleweare
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', function (req, res) {
- 
- res.render("index");
+app.get('/', async function (req, res) {
+  const photos = await Photo.find({})
+  res.render('index',{
+    photos
+  });
+});
+
+app.get('/add', function (req, res) {
+  res.render('add');
 });
 
 app.get('/about', function (req, res) {
- 
-  res.render("about");
- });
+  res.render('about');
+});
 
- app.get('/add', function (req, res) {
- 
-  res.render("add");
- });
+app.get('/contact', function (req, res) {
+  res.render('contact');
+});
 
-app.get('/about', function (req, res) {
-  
-  res.render("about");
- });
-
- app.get('/contact', function (req, res) {
-  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render("contact");
- });
+// posts
+app.post('/photo', async function (req, res) {
+ await Photo.create(req.body);
+  res.redirect('/');
+});
 
 app.listen(3000, () => {
   console.log('sunucu çalıştı');
