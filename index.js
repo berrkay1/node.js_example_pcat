5107c92c417e996190d43fd469c788ff79fc7cd0
BERKAY GÜL <berrkaygul@gmail.com>
2022-10-16 02:56:03 +0300

    crud

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override')
 const path = require('path');
 const ejs = require('ejs');
-const Photo = require("./modals/Photo");
-
+const Photo = require('./modals/Photo');
+const fs = require("fs");
 
 const app = express();
-
-
 
 //connect DB
 mongoose.connect('mongodb://localhost/pcat-test-db');
@@ -19,11 +19,16 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method',{
+  methods:['POST','GET']
+}))
 
-app.get('/', async function (req, res) {
-  const photos = await Photo.find({})
-  res.render('index',{
-    photos
+
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}).sort("-dateCreated");
+  res.render('index', {
+    photos,
   });
 });
 
@@ -41,9 +46,59 @@ app.get('/contact', function (req, res) {
 
 // posts
 app.post('/photo', async function (req, res) {
- await Photo.create(req.body);
-  res.redirect('/');
+
+  const uploadDir = "public/uploads";
+  if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir)
+  }
+
+  let uploadImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads' + uploadImage.name;
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads' + uploadImage.name,
+    });
+    res.redirect('/');
+  });
 });
+
+// photoya göre özel sayfa
+app.get('/photos/:id', async function (req, res) {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
+});
+
+app.get('/photos/edit/:id', async function (req, res) {
+  const photo = await Photo.findOne({_id:req.params.id});
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put("/photo/:id",async (req,res) => {
+  const photo = await Photo.findOne({_id:req.params.id});
+
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+
+  res.redirect(`/photos/${req.params.id}`)
+  
+})
+
+app.delete("/photos/:id",async (req,res) => {
+
+  const photo = await Photo.findOne({_id:req.params.id});
+  let deleteImage = __dirname + "/public" + photo.image;
+  fs.unlinkSync(deleteImage);
+  await Photo.findByIdAndDelete(req.params.id);
+  res.redirect("/")
+  
+})
+
 
 app.listen(3000, () => {
   console.log('sunucu çalıştı');
